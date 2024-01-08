bb067378933486e44c7ef8dbae1b948338d7fc7d
Ethan <ethann@petalmail.com>
2024-01-08 09:12:49 +0800

    :sparkles: 体系添加点击事件.

--- module/main/src/main/java/com/ethan/main/ui/fragment/FragmentSystem.kt
+++ module/main/src/main/java/com/ethan/main/ui/fragment/FragmentSystem.kt
@@ -2,11 +2,15 @@ package com.ethan.main.ui.fragment
 
 import android.os.Bundle
 import android.view.View
+import com.chad.library.adapter.base.util.setOnDebouncedItemClick
 import com.ethan.framework.base.FragmentBusinessBase
 import com.ethan.main.R
 import com.ethan.main.databinding.FragmentSystemBinding
 import com.ethan.main.ui.adapter.AdapterSystem
 import com.ethan.main.ui.viewmodel.ViewModelSystem
+import com.hjq.toast.Toaster
+import com.zackratos.ultimatebarx.ultimatebarx.java.UltimateBarX
+import io.github.uhsk.kit.asColor
 import org.koin.android.ext.android.inject
 
 private const val ARG_PARAM1 = "param1"
@@ -31,10 +35,14 @@ class FragmentSystem : FragmentBusinessBase<FragmentSystemBinding>() {
 
     override fun initView(view: View, savedInstanceState: Bundle?) {
         super.initView(view, savedInstanceState)
-        mBinding.recycler.apply {
-            adapter = mAdapter
+        UltimateBarX.statusBarOnly(this).color(com.ethan.common.R.color.white.asColor(mContext)).light(true).apply()
+        mBinding.recycler.apply { adapter = mAdapter }
+        initListener()
+    }
+
+    private fun initListener() {
+        mAdapter.setOnDebouncedItemClick { adapter, view, position ->
+            Toaster.show("点击了：${mAdapter.getItem(position)?.name}")
         }
-
-
     }
 }
